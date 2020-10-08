b65c5bf2cf951d27d9167e432b4b4052993893d8
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2020-10-08 10:23:16 +0200

    Grundgerüst

---
+++ src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+/**class Main
+ * @author Matteo Falkenberg
+ * @version 1.20, 08.10.2020
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
