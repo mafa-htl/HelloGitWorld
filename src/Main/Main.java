6a21982bafc9f22ab2a21560e7ea58290e21974d
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2020-10-08 10:30:40 +0200

    fertiges Programm nach Angaben

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.util.Scanner;
+
 /**class Main
  * @author Matteo Falkenberg
  * @version 1.20, 08.10.2020
@@ -9,7 +11,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Geben Sie 2 Werte ein, die miteinander addiert werden sollen.\n");
+
+        try {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println("Ergebnis: "+ (num1 + num2));
+        }
+        catch(Exception e) {
+            System.out.println("Sie haben einen ungültigen Wert eingegeben!");
+        }
 
     }
 
