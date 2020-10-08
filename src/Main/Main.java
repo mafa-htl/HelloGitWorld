2b40f8427f963fa6d5c314b770fc58418cd529ec
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2020-10-08 11:13:45 +0200

    hinzufügen anderer Rechenoperatoren

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,16 +9,53 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    public static boolean checkOp(String op){
+
+        if(op.equals("addieren") || op.equals("+") || op.equals("subtrahieren") || op.equals("-") || op.equals("multiplizieren") || op.equals("*") || op.equals("dividieren") || op.equals("/")) {
+            return true;
+        }
+
+        System.out.println("Ungültiger Operator");
+        return false;
+    }
+
+    public static int calculate(String op, int num1, int num2){
+
+        if(op.equals("addieren") || op.equals("+")){
+            return (num1 + num2);
+        }
+        else if(op.equals("subtrahieren") || op.equals("-")){
+            return (num1 - num2);
+        }
+        else if(op.equals("multiplizieren") || op.equals("*")){
+            return (num1 * num2);
+        }
+        else if(op.equals("dividieren") || op.equals("/")){
+            return (num1 / num2);
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Geben Sie 2 Werte ein, die miteinander addiert werden sollen.\n");
+        System.out.println("Wollen Sie 2 Zahlen: addieren (+), subtrahieren (-), multiplizieren (*) oder dividieren (/)?\n");
+
+        String operator = sc.next();
+
+        if(checkOp(operator) == false){
+            System.exit(0);
+        }
+
+        System.out.println("Geben Sie 2 Werte ein, mit denen gerechnet werden soll.\n");
 
         try {
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
-            System.out.println("Ergebnis: "+ (num1 + num2));
+            System.out.println("Ergebnis: " + calculate(operator, num1, num2));
         }
         catch(Exception e) {
             System.out.println("Sie haben einen ungültigen Wert eingegeben!");
